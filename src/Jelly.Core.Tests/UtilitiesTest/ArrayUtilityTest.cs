09142476a1a2b6d69b82890cc9bff975ff1c15e9
vanzheng <vanzheng2012@gmail.com>
2013-07-29 15:33:50 +0800

    Add unit test.

---
+++ src/Jelly.Core.Tests/UtilitiesTest/ArrayUtilityTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jelly.Utilities;
+
+namespace Jelly.Core.Tests.UtilitiesTest
+{
+    [TestClass]
+    public class ArrayUtilityTest
+    {
+        [TestMethod]
+        public void CombinTest()
+        {
+            string[] one = null;
+            string[] two = null;
+            string[] one2 = null;
+            string[] two2 = new string[] { "1" };
+            string[] one3 = new string[] { "1", "2" };
+            string[] two3 = new string[] { "3" };
+
+            string[] result = ArrayUtility.Combin<string>(one, two);
+            string[] result2 = ArrayUtility.Combin<string>(one2, two2);
+            string[] result3 = ArrayUtility.Combin<string>(one3, two3);
+
+            Assert.IsNull(result);
+            Assert.IsNotNull(result2);
+            Assert.AreEqual(1, result2.Length);
+            Assert.IsNotNull(result3);
+            Assert.AreEqual(3, result3.Length);
+        }
+    }
+}
