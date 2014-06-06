1e31854cbe9f3a51106de9acb3217fdd516e152d
vanzheng <zgbjili2009@126.com>
2014-06-06 16:03:23 +0800

    Rename formatter to Converter and refine.

---
+++ src/Jelly.Core.Tests/HelpersTest/ConverterTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Jelly.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jelly.Tests.HelpersTest
+{
+    [TestClass]
+    public class ConverterTest
+    {
+        [TestMethod]
+        public void ToComputerUnitTest()
+        {
+            ulong k0 = 1;
+            ulong k1 = 1025;
+            ulong k2 = 1024 * 1024 + 1000 * 100;
+            ulong k3 = 1024 * 1024 * 1024 + 1000 * 100 * 1024;
+            ulong k4 = 1024 * 1024 * 1024 * 1024L + 1000 * 100 * 1024 * 1024L;
+
+            string result0 = Converter.ToComputerUnit(k0);
+            string result1 = Converter.ToComputerUnit(k1);
+            string result2 = Converter.ToComputerUnit(k2);
+            string result3 = Converter.ToComputerUnit(k3);
+            string result4 = Converter.ToComputerUnit(k4);
+
+            string result00 = Converter.ToComputerUnit("#.##", k0);
+            string result01 = Converter.ToComputerUnit("#.##", k1);
+            string result02 = Converter.ToComputerUnit("#.##", k2);
+            string result03 = Converter.ToComputerUnit("#.##", k3);
+            string result04 = Converter.ToComputerUnit("#.##", k4);
+
+            Assert.AreEqual("1B", result0);
+            Assert.AreEqual("1KB", result1);
+            Assert.AreEqual("1.1MB", result2);
+            Assert.AreEqual("1.1GB", result3);
+            Assert.AreEqual("1.1TB", result4);
+
+            Assert.AreEqual("1B", result00);
+            Assert.AreEqual("1KB", result01);
+            Assert.AreEqual("1.1MB", result02);
+            Assert.AreEqual("1.1GB", result03);
+            Assert.AreEqual("1.1TB", result04);
+        }
+    }
+}
---
+++ src/Jelly.Core/Helpers/Converter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Jelly.Helpers
+{
+    /// <summary>
+    /// The converter utility.
+    /// </summary>
+    public class Converter
+    {
+        /// <summary>
+        /// Converts the data size to computer unit, like KB, MB, GB etc.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <param name="data">The data size.</param>
+        /// <returns>The computer unit string.</returns>
+        public static string ToComputerUnit(string format, ulong data)
+        {
+            if (data >= 0 && data < Constants.Kilobyte)
+            {
+                return string.Concat(data.ToString("#"), "B");
+            }
+            else if (data >= Constants.Kilobyte && data < Constants.Megabyte)
+            {
+                return string.Concat((data / Constants.Kilobyte).ToString(format), "KB");
+            }
+            else if (data >= Constants.Megabyte && data < Constants.Gigabyte)
+            {
+                return string.Concat((data / Constants.Megabyte).ToString(format), "MB");
+            }
+            else if (data >= Constants.Gigabyte && data < Constants.Terabyte)
+            {
+                return string.Concat((data / Constants.Gigabyte).ToString(format), "GB");
+            }
+            else
+            {
+                return string.Concat((data / Constants.Terabyte).ToString(format), "TB");
+            }
+        }
+
+        /// <summary>
+        /// Converts the data size to computer unit, like KB, MB, GB etc.
+        /// </summary>
+        /// <param name="data">The data size.</param>
+        /// <returns>The computer unit string.</returns>
+        public static string ToComputerUnit(ulong data)
+        {
+            return ToComputerUnit("#.#", data);
+        }
+    }
+}
